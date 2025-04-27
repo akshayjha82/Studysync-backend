63fb8321f8c7393656de991c87b8de73d7743ffd
akshayjha82 <akshayjha8263@gmail.com>
2025-04-27 23:43:37 +0530

    resolved

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const io = new Server(server,
      { 
         cors: 
         { 
-            origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+            origin: ["http://localhost:5173", "https://studysync-frontend.vercel.app"],
             methods: ["GET", "POST"] 
         } 
     });
